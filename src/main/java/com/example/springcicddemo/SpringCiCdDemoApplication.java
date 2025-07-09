a1f4c83cb33cf285dfaae1f8a06b0e087e373f49
Aykhan Mammadli <mmmdliayxan2107@gmail.com>
2025-07-09 20:03:30 +0400

    first commit

---
+++ src/main/java/com/example/springcicddemo/SpringCiCdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCiCdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCiCdDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcicddemo/SpringCiCdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCiCdDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
